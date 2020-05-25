90d34a53b418fe872c7df6ee6915069e69288306
oliver <dopplereffect1992@gmail.com>
2020-05-25 21:01:44 +0100

    Add project files.

---
+++ webapp/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace webapp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+         Web.Source = "https://mardplay.com/feed";
+        }
+
+        async void OnBackButtonClicked(object sender, EventArgs e)
+        {
+            if (Web.CanGoBack)
+            {
+                Web.GoBack();
+            }
+            else
+            {
+                await Navigation.PopAsync();
+            }
+        }
+
+        void OnForwardButtonClicked(object sender, EventArgs e)
+        {
+            if (Web.CanGoForward)
+            {
+                Web.GoForward();
+            }
+        }
+    }
+}
